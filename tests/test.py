a86b310d608dcce1c921bd5e44b2427c4293e551
Juan Sebastian Avila <sebas.aviloncho@gmail.com>
2021-04-13 16:37:37 -0500

    Create fixtures for test. Create test_big_dna on test.py.

--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,8 @@
 """Test Mutant"""
 import unittest
+
 import mutant
+import fixtures
 
 
 class TestMutant(unittest.TestCase):
@@ -10,58 +12,33 @@ class TestMutant(unittest.TestCase):
         Test human DNA example with \
         3 sequences (Diagonal, horizontal, vertical).
         """
-        dna = [
-            'ATGCGA',
-            'CAGTGC',
-            'TTATGT',
-            'AGAAGG',
-            'CCCCTA',
-            'TCACTG'
-        ]
 
-        self.assertTrue(mutant.is_mutant(dna))
+        self.assertTrue(mutant.is_mutant(fixtures.EXAMPLE))
 
     def test_is_mutant_2_diagonals(self):
         """
         Test human DNA with 2 sequences (Diagonal, anti-diagonal).
         """
-        dna = [
-            'TTGCGC',
-            'CAGTCC',
-            'ATTCGT',
-            'AACAGG',
-            'CCACTA',
-            'TCAATG'
-        ]
 
-        self.assertTrue(mutant.is_mutant(dna))
+        self.assertTrue(mutant.is_mutant(fixtures.DIAGONAL2))
 
     def test_is_mutant_1_seq(self):
         """
         Test human DNA with 1 sequence (Vertical).
         """
-        dna = [
-            'ATGCGA',
-            'CCGTTC',
-            'TTATGG',
-            'AGAAGG',
-            'CAGCTG',
-            'TCACTG'
-        ]
 
-        self.assertFalse(mutant.is_mutant(dna))
+        self.assertFalse(mutant.is_mutant(fixtures.VERTICAL1))
 
     def test_is_mutant_human(self):
         """
         Test human DNA without sequences.
         """
-        dna = [
-            'CTGCTA',
-            'CAGTGC',
-            'TTCTGT',
-            'AGAAGG',
-            'CGCCTA',
-            'TCACTG'
-        ]
 
-        self.assertFalse(mutant.is_mutant(dna))
+        self.assertFalse(mutant.is_mutant(fixtures.HUMAN))
+
+    def test_big_dna(self):
+        """
+        Test big human DNA.
+        """
+
+        self.assertTrue(mutant.is_mutant(fixtures.BIGDNA))
